831511e234832197c95c52e30ee9d2e822b7b056
dc7batman <mohan.dc7batman@gmail.com>
2016-08-20 22:22:15 +0530

    Create main viewcontroller

---
+++ BucketList/BucketList/BLMainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BLMainViewController.swift
+//  BucketList
+//
+//  Created by Mohan on 20/08/16.
+//  Copyright © 2016 bucketlistapp. All rights reserved.
+//
+
+import UIKit
+
+class BLMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
